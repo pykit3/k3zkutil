3d1eda4c00356beda40df6f09c0d2471e975a21d
IloveKanade <xjlgxlgx@gmail.com>
2021-08-17 11:46:30 +0800

    new-feature:add k3zkutil

--- test/test_doctest.py
+++ test/test_doctest.py
@@ -1,8 +1,8 @@
 import doctest
 
-import {{ name }}
+import k3zkutil
 
 
 def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite({{ name }}))
+    tests.addTests(doctest.DocTestSuite(k3zkutil))
     return tests
